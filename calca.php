e3c6cc4d60015bbb1c3e03638ddbdc7f0abb2260
Joao Marcus Cardoso <rfcjoujou1@gmail.com>
2018-08-13 00:39:04 -0300

    Foi feito os redicionamento, e criado a pasta de calças

---
+++ calca.php
@@ -0,0 +1,108 @@
+<!doctype html>
+<html>
+	<head>
+
+		<meta charset="UTF-8" / >
+		<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no"/>
+		<!-- Bootstrap colocado -->
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="assets/css/style_calca.css" />
+		
+		<!-- arquivo do javascript -->
+		<script type="text/javascript" src="assets/js/script.js"></script>
+		<title>Arrazo_Blend</title>
+	</head>
+	<body>
+
+
+		<div class="container-fluid">
+			<div class="row">
+					
+				<div class="topo">
+
+		            <div class="topoint">
+		                <div class="topoleft"> <!-- foi colocado na esquerda -->
+		                    <div class="acesso">
+		                        <strong>Login </strong>
+		                        <img src="assets/images/down.png" border="0" width="10" height="10"/> 
+		                        
+
+		                    </div>
+
+		                </div>
+		            </div>
+
+		            
+		            <div class="toporight"> <!-- foi colocado na esquerda pois ficou melhor -->                       
+		                <div class="toporight-rede">  <!--REDE SOCIAIS -->
+
+		                    <ul>
+
+		                        <a href="" OnMouseOver="emcima()" class="fundo"><img src="assets/images/whatsap.png" border="0" widht="23" height="25" /></a>
+		                        <a href="https://www.instagram.com/arrazoblend/?hl=pt-br" OnMouseOver="emcima()" class="fundo"><img src="assets/images/instagram.png" border="0" widht="23" height="25"/></a>
+		                    </ul>
+
+		                </div>
+
+		            </div>
+		        </div>
+	       	</div>
+
+	       	<!-- menunav -->
+
+	    	<div class="row">
+				<nav class="navbar navbar-expand-lg navbar-dark" id="menunav">
+
+					<a class="navbar-brand" id="logonav" href="#">ArrazO Blend</a>
+
+					<button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+
+					<div class="navbar-collapse collapse" id="navbarMenu">
+
+						<div class="navbar-nav">
+							<a href="lancamento.php" class="nav-item nav-link">Lançamento</a>
+							<a href="calca.php" class="nav-item nav-link  ">Calça</a>
+							<a href="#" class="nav-item nav-link ">Shorts </a>
+							<a href="#" class="nav-item nav-link ">Promoções</a>
+							<a href="#" class="nav-item nav-link ">Quem somos </a>
+
+
+						</div>
+					</div>
+
+						<form method="POST" class="form-inline" id="icones">
+
+
+
+							<!-- Icones de Busca -->
+							<input type="text" class="form-control" placeholder="Pesquisar..." id="pesquisaa"/>
+							
+							
+							<button type="submit" class="search-input-form" > 
+								<img src="assets/images/luppa.png" />
+							</button>
+
+		
+
+
+					
+							
+
+								
+							
+
+						</form>
+						
+							
+						
+							
+				</nav>
+			</div>
+
+
+		<script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
+		<script type="text/javascript" src="assets/js/bootstrap.bundle.min.js"></script>
+	</body>
+</html>
