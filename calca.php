4a0bb63ce3804e4cb42b0ddf122293d574dbe37b
Joao Marcus Cardoso <rfcjoujou1@gmail.com>
2018-08-15 21:17:25 -0300

    Foi ajustada a pasta calca.php

--- calca.php
+++ calca.php
@@ -101,6 +101,176 @@
 				</nav>
 			</div>
 
+		</div>
+		<div class="container-fluid">
+			<!-- Conteúdo -->
+		
+			
+
+			<div class="row" style="margin-top:50px;">
+				
+				<div class="row">
+					<div class="col-6"> 
+						<img src="assets/images/slideshow/principal.png" width="100%" />
+					</div>
+
+					<div class="col">
+								b
+					</div>
+
+					<div class="col">
+								
+
+					</div>
+				</div>
+
+			</div>
+
+
+		</div>
+		
+
+		<div class="container-fluid">
+			<div class="row">
+				<!--Deixamos ele em inativo para depois ajeitar--> 
+				<footer class="page-footer font-small teal pt-4" style="width:100%;">
+
+	    			<!-- Footer Text -->
+	    			<div class="container-fluid text-center text-md-left" id="footerint" style="background-color:white;">
+	    				<div class="row">
+	    					<div class="col-sm" style="color:white">
+
+	    						<!-- Primeiro bloco do footer -->
+
+	    						<div class="tituloFooter" >
+		    						<h5 style="font-size:20px;">Pagamento</h5>
+		    						<img src=""/>
+		    					</div>
+
+		    					<div class="tituloFooter2">
+
+		    						<h5 class="text-uppercase" style="font-size:20px;">Compra segura</h5>
+		    						<p >E oq</p>
+		    					</div>
+
+		    					<div class="tituloFooter3">
+
+		    						<h5 class="text-uppercase" style="font-size:18px;">Descontos</h5>
+
+
+		    					</div>
+
+		    				</div>
+		    				<div class="col-sm" style="color:white">
+		    					<!-- Segundo bloco do footer -->
+		    					<div class="tituloFooter">
+
+		    						<h5 style="text-uppercase;font-size:20px;" >Duvida</h5>
+		    						<p style="text-transform:none;" id="itemfooterp"><a href="#" style="text-decoration:none;color:white;">Frete/prazo</a></p>
+		    						
+			    					<p style="text-transform:none;" id="itemfooter"><a href="#" style="text-decoration:none;color:white;">Lojas</a></p>
+
+			    					<p style="text-transform:none;" id="itemfooter"><a href="#" style="text-decoration:none;color:white;">Sobre</a></p>
+
+			    					<p style="text-transform:none;" id="itemfooter"><a href="#" style="text-decoration:none;color:white;">Como comprar</a></p>
+			    					
+		    						
+
+		    					</div>
+
+		    				
+		    				</div>
+
+
+		    				<div class="col-sm" style="color:white">
+		    					<!-- Penultimo bloco do footer -->
+
+		    					<div class="tituloFooter">
+		    						<h5 class="text-uppercase" style="font-size:20px;">Pagamento</h5>
+		    						
+		    						<p >b</p>
+		    						
+
+		    					</div>
+
+		    					<div class="tituloFooter2" >
+
+		    						<h5 class="text-uppercase" style="font-size:20px;"><a href="" style="color:white">Trocas e Devoluções</a></h5>
+
+		    						<div class="footersubitem" style="background-color:white;">
+
+		    						</div>
+
+		    					</div>
+
+		    					<div class="tituloFooter3">
+
+
+		    						<h5 class="text-uppercase" style="font-size:20px;">Pagamento</h5>
+		    						<div class="footersubitem" style="background-color:white;">
+
+
+		    						</div>
+		    					</div>
+		    				</div>
+
+		    					<!-- Ultimo bloco do footer -->
+		    				<div class="col-sm" style="color:white">
+		    					<!-- Para enviar notificação -->
+		    					<div class="tituloFooter">
+
+		    						<h5 class="text-uppercase" style="font-size:18px;;">Notificações</h5>
+		    						<div class="footersubitem" style="background-color:#110d0d;">
+			    						<input type="text" placeholder="Seu e-mail..." name="assinatura" id="assinatura"/>
+			    						<input type="submit" value="Assine"/>
+			    					</div>
+		    					</div>
+
+		    					<div class="tituloFooter2">
+
+		    						<h5 class="text-uppercase" style="font-size:20px;">Contatos</h5>
+		    						
+
+		    						<a href="https://www.instagram.com/arrazoblend/?hl=pt-br"><img src="assets/images/instagraam.png"/></a>
+		    						<p></p>
+
+		    						
+		    						
+
+		    					</div>
+
+
+		    					<div class="tituloFooter3">
+
+		    						<h5 class="text-uppercase" style="font-size:20px;">....</h5>
+
+
+		    					</div>
+
+		    					
+		    				</div>
+
+		    			</div>
+
+		    			<!-- Segunda linha de endereço -->
+		    			<div class="row">
+		    				<div class="col-sm" id="enderecoint">
+		    					<div class="endereco">
+		    						<p>Rua major Joaquim A. De Campos 5810 -Santo Amaro da Imperatriz, SC-CEP: 88140000</p>
+		    					<div class="email">
+		    						<p>Contato: arrazo.blendd@gmail.com</p>
+		    					<div>
+		    					</div>
+
+		    				</div>
+		    			</div>
+		    		</div>
+
+				</footer>
+			</div>
+
+		</div>
+
 
 		<script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
 		<script type="text/javascript" src="assets/js/bootstrap.bundle.min.js"></script>
